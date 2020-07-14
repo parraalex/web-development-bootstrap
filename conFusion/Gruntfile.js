765e115080092f7e0d127de8109d0d89d2d2305b
Alejandro Parra <A01229618@itesm.mx>
2020-07-14 10:24:05 -0500

    added grunt support

---
+++ conFusion/Gruntfile.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = function(grunt) {
+    require('time-grunt')(grunt);
+    require('jit-grunt')(grunt);
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'css/styles.css': 'css/styles.scss'
+                }
+            }
+        },
+        watch:{
+            files: 'css/*.scss',
+            tasks: ['tasks']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'css/*.scss',
+                        '*.html',
+                        'js/*.js'
+                    ]
+
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './'
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('css',['sass']);
+    grunt.registerTask('default',['browserSync','watch']);
+}
